3e1349cd38f9331aee362939a68440d04f96fe88
Lucas Monteiro <lucas.monteiro58@gmail.com>
2026-03-09 13:36:30 -0300

    feat: add netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -14,6 +15,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    netlify(),
   ],
 })
 
